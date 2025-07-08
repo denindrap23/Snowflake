dd1c732543c23293d400167753308883f731713b
Deni Indra Permana <73006024+denindrap23@users.noreply.github.com>
2025-07-08 22:03:58 +0700

    Update 09.Snowpipe.sql

--- Tips_and_Tricks/09.Snowpipe.sql
+++ Tips_and_Tricks/09.Snowpipe.sql
@@ -1,6 +1,6 @@
-// 9.1 Create Stage and Pipe
+-- 9.1 Create Stage and Pipe
   
-// Create table first
+-- Create table first
 CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.employees (
     id INT,
     first_name STRING,
@@ -10,7 +10,7 @@ CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.employees (
     department STRING
     );
     
-// Create file format object
+-- Create file format object
 CREATE OR REPLACE file format MANAGE_DB.file_formats.csv_fileformat
     type = csv
     field_delimiter = ','
@@ -18,50 +18,50 @@ CREATE OR REPLACE file format MANAGE_DB.file_formats.csv_fileformat
     null_if = ('NULL','null')
     empty_field_as_null = TRUE;
     
-// Create stage object with integration object & file format object
+-- Create stage object with integration object & file format object
 CREATE OR REPLACE stage MANAGE_DB.external_stages.csv_folder
-    URL = 's3://snowflakes3bucket123/csv/snowpipe'
+    URL = 's3:--snowflakes3bucket123/csv/snowpipe'
     STORAGE_INTEGRATION = s3_int
     FILE_FORMAT = MANAGE_DB.file_formats.csv_fileformat
    
-// Create stage object with integration object & file format object
+-- Create stage object with integration object & file format object
 LIST @MANAGE_DB.external_stages.csv_folder;
 
-// Create schema to keep things organized
+-- Create schema to keep things organized
 CREATE OR REPLACE SCHEMA MANAGE_DB.pipes;
 
-// Define pipe
+-- Define pipe
 CREATE OR REPLACE pipe MANAGE_DB.pipes.employee_pipe
     auto_ingest = TRUE
     AS
     COPY INTO OUR_FIRST_DB.PUBLIC.employees
 FROM @MANAGE_DB.external_stages.csv_folder; 
 
-// Describe Pipe
+-- Describe Pipe
 DESC pipe employee_pipe;
     
 SELECT * FROM OUR_FIRST_DB.PUBLIC.employees;
 
 
-// 9.2 Create Pipe
+-- 9.2 Create Pipe
 
-// Define pipe
+-- Define pipe
 CREATE OR REPLACE pipe MANAGE_DB.pipes.employee_pipe
     auto_ingest = TRUE
     AS
     COPY INTO OUR_FIRST_DB.PUBLIC.employees
 FROM @MANAGE_DB.external_stages.csv_folder; 
 
-// Describe pipe
+-- Describe pipe
 DESC pipe employee_pipe;
     
 SELECT * FROM OUR_FIRST_DB.PUBLIC.employees;
 
 
-// 9.3 Error Handling
+-- 9.3 Error Handling
   
-// Handling errors
-// Create file format object
+-- Handling errors
+-- Create file format object
 CREATE OR REPLACE file format MANAGE_DB.file_formats.csv_fileformat
     type = csv
     field_delimiter = ','
@@ -73,21 +73,21 @@ SELECT * FROM OUR_FIRST_DB.PUBLIC.employees;
 
 ALTER PIPE employee_pipe refresh;
  
-// Validate pipe is actually working
+-- Validate pipe is actually working
 SELECT SYSTEM$PIPE_STATUS('employee_pipe');
 
-// Snowpipe error message
+-- Snowpipe error message
 SELECT * FROM TABLE(VALIDATE_PIPE_LOAD(
     PIPE_NAME => 'MANAGE_DB.pipes.employee_pipe',
     START_TIME => DATEADD(HOUR,-2,CURRENT_TIMESTAMP())));
 
-// COPY command history from table to see error massage
+-- COPY command history from table to see error massage
 SELECT * FROM TABLE (INFORMATION_SCHEMA.COPY_HISTORY(
    table_name  =>  'OUR_FIRST_DB.PUBLIC.EMPLOYEES',
    START_TIME =>DATEADD(HOUR,-2,CURRENT_TIMESTAMP())));
 
 
-// 9.3 Manage Pipe
+-- 9.3 Manage Pipe
 -- Manage pipes -- 
 DESC pipe MANAGE_DB.pipes.employee_pipe;
 
@@ -102,7 +102,7 @@ SHOW PIPES IN SCHEMA MANAGE_DB.pipes;
 SHOW PIPES LIKE '%employee%' IN Database MANAGE_DB;
 
 -- Changing pipe (alter stage or file format) --
-// Preparation table first
+-- Preparation table first
 CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.employees2 (
     id INT,
     first_name STRING,
@@ -112,13 +112,13 @@ CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.employees2 (
     department STRING
     );
 
-// Pause pipe
+-- Pause pipe
 ALTER PIPE MANAGE_DB.pipes.employee_pipe SET PIPE_EXECUTION_PAUSED = true;
  
-// Verify pipe is paused and has pendingFileCount 0 
+-- Verify pipe is paused and has pendingFileCount 0 
 SELECT SYSTEM$PIPE_STATUS('MANAGE_DB.pipes.employee_pipe');
  
- // Recreate the pipe to change the COPY statement in the definition
+ -- Recreate the pipe to change the COPY statement in the definition
 CREATE OR REPLACE pipe MANAGE_DB.pipes.employee_pipe
     auto_ingest = TRUE
     AS
@@ -127,17 +127,17 @@ FROM @MANAGE_DB.external_stages.csv_folder
 
 ALTER PIPE  MANAGE_DB.pipes.employee_pipe refresh;
 
-// List files in stage
+-- List files in stage
 LIST @MANAGE_DB.external_stages.csv_folder; 
 
 SELECT * FROM OUR_FIRST_DB.PUBLIC.employees2;
 
- // Reload files manually that where aleady in the bucket
+ -- Reload files manually that where aleady in the bucket
 COPY INTO OUR_FIRST_DB.PUBLIC.employees2
 FROM @MANAGE_DB.external_stages.csv_folder  
 
-// Resume pipe
+-- Resume pipe
 ALTER PIPE MANAGE_DB.pipes.employee_pipe SET PIPE_EXECUTION_PAUSED = false;
 
-// Verify pipe is running again
+-- Verify pipe is running again
 SELECT SYSTEM$PIPE_STATUS('MANAGE_DB.pipes.employee_pipe'); 
