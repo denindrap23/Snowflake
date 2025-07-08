f86a558f468188b1a21f294ac7efd6e77e16c1e4
Deni Indra Permana <73006024+denindrap23@users.noreply.github.com>
2025-07-08 22:05:31 +0700

    Update 10.Time_Travel.sql

--- Tips_and_Tricks/10.Time_Travel.sql
+++ Tips_and_Tricks/10.Time_Travel.sql
@@ -1,6 +1,6 @@
-// 10.1 Using Time Travel
+-- 10.1 Using Time Travel
 
-// Setting up table
+-- Setting up table
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test (
     id INT,
     first_name STRING,
@@ -17,7 +17,7 @@ CREATE OR REPLACE FILE FORMAT MANAGE_DB.file_formats.csv_file
     skip_header = 1;
     
 CREATE OR REPLACE STAGE MANAGE_DB.external_stages.time_travel_stage
-    URL = 's3://data-snowflake-fundamentals/time-travel/'
+    URL = 's3:--data-snowflake-fundamentals/time-travel/'
     file_format = MANAGE_DB.file_formats.csv_file;
     
 LIST @MANAGE_DB.external_stages.time_travel_stage;
@@ -28,14 +28,14 @@ files = ('customers.csv')
 
 SELECT * FROM OUR_FIRST_DB.public.test;
 
-// Use-case: Update data (by mistake)
+-- Use-case: Update data (by mistake)
 UPDATE OUR_FIRST_DB.public.test
 SET FIRST_NAME = 'Joyen';
 
-// Using time travel: Method 1 - 2 minutes back
+-- Using time travel: Method 1 - 2 minutes back
 SELECT * FROM OUR_FIRST_DB.public.test at (OFFSET => -60*1.5);
 
-// Using time travel: Method 2 - before timestamp
+-- Using time travel: Method 2 - before timestamp
 SELECT * FROM OUR_FIRST_DB.public.test before (timestamp => '2025-07-07 17:47:50.581'::timestamp);
 
 -- Setting up table
@@ -66,8 +66,8 @@ SELECT * FROM OUR_FIRST_DB.public.test;
 
 SELECT * FROM OUR_FIRST_DB.public.test before (timestamp => '2025-07-08 07:30:47.145'::timestamp)
 
-// Using time travel: Method 3 - before Query ID
-// Preparing table
+-- Using time travel: Method 3 - before Query ID
+-- Preparing table
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test (
     id INT,
     first_name STRING,
@@ -84,7 +84,7 @@ files = ('customers.csv');
 
 SELECT * FROM OUR_FIRST_DB.public.test;
 
-// Altering table (by mistake)
+-- Altering table (by mistake)
 UPDATE OUR_FIRST_DB.public.test
 SET EMAIL = null;
 
@@ -93,9 +93,9 @@ SELECT * FROM OUR_FIRST_DB.public.test;
 SELECT * FROM OUR_FIRST_DB.public.test before (statement => '019b9ee5-0500-8473-0043-4d8300073062');
 
 
-// 10.2 Restoring in Time Travel
+-- 10.2 Restoring in Time Travel
 
-// Use-case: Update data (by mistake)
+-- Use-case: Update data (by mistake)
 UPDATE OUR_FIRST_DB.public.test
 SET LAST_NAME = 'Tyson';
 
@@ -104,7 +104,7 @@ SET JOB = 'Data Analyst';
 
 SELECT * FROM OUR_FIRST_DB.public.test before (statement => '019b9eea-0500-845a-0043-4d830007402a');
 
-// Bad method
+-- Bad method
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test as
 SELECT * FROM OUR_FIRST_DB.public.test before (statement => '019b9eea-0500-845a-0043-4d830007402a');
 
@@ -113,8 +113,7 @@ SELECT * FROM OUR_FIRST_DB.public.test;
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test as
 SELECT * FROM OUR_FIRST_DB.public.test before (statement => '019b9eea-0500-8473-0043-4d830007307a');
 
-// Good method
-
+-- Good method
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.test_backup AS
 SELECT * FROM OUR_FIRST_DB.public.test before (statement => '019b9ef0-0500-8473-0043-4d830007309a');
 
@@ -126,40 +125,36 @@ SELECT * FROM OUR_FIRST_DB.public.test_backup;
 SELECT * FROM OUR_FIRST_DB.public.test;
 
 
-// 10.3 Undrop Table
-
-// UNDROP command - Tables
-
+-- 10.3 Undrop Table
+-- UNDROP command - Tables
 DROP TABLE OUR_FIRST_DB.public.customers;
 
 SELECT * FROM OUR_FIRST_DB.public.customers;
 
 UNDROP TABLE OUR_FIRST_DB.public.customers;
 
-// UNDROP command - Schemas
-
+-- UNDROP command - Schemas
 DROP SCHEMA OUR_FIRST_DB.public;
 
 SELECT * FROM OUR_FIRST_DB.public.customers;
 
 UNDROP SCHEMA OUR_FIRST_DB.public;
 
-// UNDROP command - Database
-
+-- UNDROP command - Database
 DROP DATABASE OUR_FIRST_DB;
 
 SELECT * FROM OUR_FIRST_DB.public.customers;
 
 UNDROP DATABASE OUR_FIRST_DB;
 
-// Restore replaced table 
+-- Restore replaced table 
 UPDATE OUR_FIRST_DB.public.customers
 SET LAST_NAME = 'Tyson';
 
 UPDATE OUR_FIRST_DB.public.customers
 SET JOB = 'Data Analyst';
 
-// Undroping a with a name that already exists
+-- Undroping a with a name that already exists
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.customers as
 SELECT * FROM OUR_FIRST_DB.public.customers before (statement => '019b9f7c-0500-851b-0043-4d83000762be')
 
@@ -173,18 +168,18 @@ RENAME TO OUR_FIRST_DB.public.customers_wrong;
 DESC table OUR_FIRST_DB.public.customers;
 
 
-// 10.4 Time Travel Cost
+-- 10.4 Time Travel Cost
 
 SELECT * FROM SNOWFLAKE.ACCOUNT_USAGE.STORAGE_USAGE ORDER BY USAGE_DATE DESC;
 
 SELECT * FROM SNOWFLAKE.ACCOUNT_USAGE.TABLE_STORAGE_METRICS;
 
-// Query time travel storage
+-- Query time travel storage
 SELECT 	ID, 
-		TABLE_NAME, 
-		TABLE_SCHEMA,
+	TABLE_NAME, 
+	TABLE_SCHEMA,
         TABLE_CATALOG,
-		ACTIVE_BYTES / (1024*1024*1024) AS STORAGE_USED_GB,
-		TIME_TRAVEL_BYTES / (1024*1024*1024) AS TIME_TRAVEL_STORAGE_USED_GB
+	ACTIVE_BYTES / (1024*1024*1024) AS STORAGE_USED_GB,
+	TIME_TRAVEL_BYTES / (1024*1024*1024) AS TIME_TRAVEL_STORAGE_USED_GB
 FROM SNOWFLAKE.ACCOUNT_USAGE.TABLE_STORAGE_METRICS
 ORDER BY STORAGE_USED_GB DESC,TIME_TRAVEL_STORAGE_USED_GB DESC;
