2d63deb4b92e67e7c3c8dc5780ac133153d45b3a
Deni Indra Permana <73006024+denindrap23@users.noreply.github.com>
2025-07-08 22:08:43 +0700

    Update 13.Zero_Copy_Cloning.sql

--- Tips_and_Tricks/13.Zero_Copy_Cloning.sql
+++ Tips_and_Tricks/13.Zero_Copy_Cloning.sql
@@ -1,6 +1,6 @@
-// 13.1 Cloning Schema & Databases
+-- 13.1 Cloning Schema & Databases
   
-// Cloning Schema
+-- Cloning Schema
 CREATE TRANSIENT SCHEMA OUR_FIRST_DB.COPIED_SCHEMA
 CLONE OUR_FIRST_DB.PUBLIC;
 
@@ -9,7 +9,7 @@ SELECT * FROM COPIED_SCHEMA.CUSTOMERS;
 CREATE TRANSIENT SCHEMA OUR_FIRST_DB.EXTERNAL_STAGES_COPIED
 CLONE MANAGE_DB.EXTERNAL_STAGES;
 
-// Cloning Database
+-- Cloning Database
 CREATE TRANSIENT DATABASE OUR_FIRST_DB_COPY
 CLONE OUR_FIRST_DB;
 
@@ -18,9 +18,9 @@ DROP SCHEMA OUR_FIRST_DB.EXTERNAL_STAGES_COPIED
 DROP SCHEMA OUR_FIRST_DB.COPIED_SCHEMA
 
 
-// 13.2 Cloning with Time Travel
+-- 13.2 Cloning with Time Travel
 
-// Setting up table
+-- Setting up table
 CREATE OR REPLACE TABLE OUR_FIRST_DB.public.time_travel (
     id INT,
     first_name STRING,
@@ -37,7 +37,7 @@ CREATE OR REPLACE FILE FORMAT MANAGE_DB.file_formats.csv_file
     skip_header = 1;
     
 CREATE OR REPLACE STAGE MANAGE_DB.external_stages.time_travel_stage
-    URL = 's3://data-snowflake-fundamentals/time-travel/'
+    URL = 's3:--data-snowflake-fundamentals/time-travel/'
     file_format = MANAGE_DB.file_formats.csv_file;
     
 LIST @MANAGE_DB.external_stages.time_travel_stage;
@@ -48,26 +48,26 @@ files = ('customers.csv');
 
 SELECT * FROM OUR_FIRST_DB.public.time_travel;
 
-// Update data
+-- Update data
 
 UPDATE OUR_FIRST_DB.public.time_travel
 SET FIRST_NAME = 'Frank';
 
-// Using time travel
+-- Using time travel
 SELECT * FROM OUR_FIRST_DB.public.time_travel AT (OFFSET => -60*1);
 
-// Using time travel
+-- Using time travel
 CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.time_travel_clone
 CLONE OUR_FIRST_DB.public.time_travel AT (OFFSET => -60*1.5);
 
 SELECT * FROM OUR_FIRST_DB.PUBLIC.time_travel_clone;
 
-// Update data again
+-- Update data again
 
 UPDATE OUR_FIRST_DB.public.time_travel_clone
 SET JOB = 'Snowflake Analyst';
 
-// Using time travel: Method 2 - before Query
+-- Using time travel: Method 2 - before Query
 SELECT * FROM OUR_FIRST_DB.public.time_travel_clone BEFORE (statement => '<your-query-id>');
 
 CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.time_travel_clone_of_clone
