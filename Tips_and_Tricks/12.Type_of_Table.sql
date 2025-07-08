83f32533845e4c1e7a2a352e1dc22262aa74323a
Deni Indra Permana <73006024+denindrap23@users.noreply.github.com>
2025-07-08 20:01:18 +0700

    Create 12.Type_of_Table.sql

---
+++ Tips_and_Tricks/12.Type_of_Table.sql
@@ -0,0 +1,181 @@
+// 12.1 Permanent Tables & Database
+
+CREATE OR REPLACE DATABASE PDB;
+
+CREATE OR REPLACE TABLE PDB.public.customers (
+    id INT,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
+    );
+  
+CREATE OR REPLACE TABLE PDB.public.helper (
+    id INT,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
+    );
+    
+// Stage and file format
+CREATE OR REPLACE FILE FORMAT MANAGE_DB.file_formats.csv_file
+    type = csv
+    field_delimiter = ','
+    skip_header = 1
+    
+CREATE OR REPLACE STAGE MANAGE_DB.external_stages.time_travel_stage
+    URL = 's3://data-snowflake-fundamentals/time-travel/'
+    file_format = MANAGE_DB.file_formats.csv_file;
+    
+LIST  @MANAGE_DB.external_stages.time_travel_stage;
+
+// Copy data and insert in table
+COPY INTO PDB.public.helper
+FROM @MANAGE_DB.external_stages.time_travel_stage
+files = ('customers.csv');
+
+SELECT * FROM PDB.public.helper;
+
+INSERT INTO PDB.public.customers
+SELECT
+    t1.ID,
+    t1.FIRST_NAME,
+    t1.LAST_NAME,
+    t1.EMAIL,
+    t1.GENDER,
+    t1.JOB,t1.PHONE
+FROM PDB.public.helper t1
+CROSS JOIN (SELECT * FROM PDB.public.helper) t2
+CROSS JOIN (SELECT TOP 100 * FROM PDB.public.helper) t3;
+
+// Show table and validate
+SHOW TABLES;
+
+
+// 12.2 Transient Tables
+CREATE OR REPLACE DATABASE TDB;
+
+CREATE OR REPLACE TRANSIENT TABLE TDB.public.customers_transient (
+    id INT,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
+    );
+
+INSERT INTO TDB.public.customers_transient
+SELECT t1.* FROM OUR_FIRST_DB.public.customers t1
+CROSS JOIN (SELECT * FROM OUR_FIRST_DB.public.customers) t2
+
+SHOW TABLES;
+
+// Query storage
+SELECT * FROM SNOWFLAKE.ACCOUNT_USAGE.TABLE_STORAGE_METRICS
+
+SELECT 	
+    ID, 
+    TABLE_NAME, 
+		TABLE_SCHEMA,
+    TABLE_CATALOG,
+		ACTIVE_BYTES,
+		TIME_TRAVEL_BYTES / (1024*1024*1024) AS TIME_TRAVEL_STORAGE_USED_GB,
+		FAILSAFE_BYTES / (1024*1024*1024) AS FAILSAFE_STORAGE_USED_GB,
+        IS_TRANSIENT,
+        DELETED,
+        TABLE_CREATED,
+        TABLE_DROPPED,
+        TABLE_ENTERED_FAILSAFE
+FROM SNOWFLAKE.ACCOUNT_USAGE.TABLE_STORAGE_METRICS
+WHERE TABLE_CATALOG ='TDB'
+ORDER BY TABLE_CREATED DESC;
+
+// Set retention time to 0
+ALTER TABLE TDB.public.customers_transient
+SET DATA_RETENTION_TIME_IN_DAYS  = 0;
+
+DROP TABLE TDB.public.customers_transient;
+
+UNDROP TABLE TDB.public.customers_transient;
+
+SHOW TABLES;
+
+// Creating transient schema and then table 
+CREATE OR REPLACE TRANSIENT SCHEMA TRANSIENT_SCHEMA;
+
+SHOW SCHEMAS;
+
+CREATE OR REPLACE TABLE TDB.TRANSIENT_SCHEMA.new_table (
+    id INT,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
+    );
+  
+ALTER TABLE TDB.TRANSIENT_SCHEMA.new_table
+SET DATA_RETENTION_TIME_IN_DAYS  = 2
+
+SHOW TABLES;
+
+
+// 12.3 Temporary Tables
+
+USE DATABASE PDB;
+
+// Create permanent table 
+CREATE OR REPLACE TABLE PDB.public.customers (
+    id INT,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
+    );
+
+INSERT INTO PDB.public.customers
+SELECT t1.* FROM OUR_FIRST_DB.public.customers t1;
+
+SELECT * FROM PDB.public.customers;
+
+// Create temporary table (with the same name)
+CREATE OR REPLACE TEMPORARY TABLE PDB.public.customers (
+    id INT,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
+    );
+
+// Validate temporary table is the active table
+SELECT * FROM PDB.public.customers;
+
+// Create second temporary table (with a new name)
+CREATE OR REPLACE TEMPORARY TABLE PDB.public.temp_table (
+    id INT,
+    first_name STRING,
+    last_name STRING,
+    email STRING,
+    gender STRING,
+    Job STRING,
+    Phone STRING
+    );
+
+// Insert data in the new table
+INSERT INTO PDB.public.temp_table
+SELECT * FROM PDB.public.customers;
+
+SELECT * FROM PDB.public.temp_table;
+
+SHOW TABLES;
