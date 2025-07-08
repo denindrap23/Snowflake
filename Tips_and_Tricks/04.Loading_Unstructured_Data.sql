8d444f6db4afd10bd14d2bdedd817a907620fd3a
Deni Indra Permana <73006024+denindrap23@users.noreply.github.com>
2025-07-08 18:57:51 +0700

    Update and rename Loading_Unstructured_Data.sql to 04.Loading_Unstructured_Data.sql

--- Tips_and_Tricks/04.Loading_Unstructured_Data.sql
+++ Tips_and_Tricks/04.Loading_Unstructured_Data.sql
@@ -169,3 +169,128 @@ SELECT
 FROM OUR_FIRST_DB.PUBLIC.JSON_RAW, TABLE(flatten(RAW_FILE:spoken_languages)) f;
 
 SELECT * FROM Languages;
+
+
+// 4.6 Querying PARQUET Data
+
+// Create file format and stage object  
+CREATE OR REPLACE FILE FORMAT MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT
+    TYPE = 'parquet';
+
+CREATE OR REPLACE STAGE MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+    url = 's3://snowflakeparquetdemo'   
+    FILE_FORMAT = MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT;
+
+// Preview the data
+LIST  @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;   
+    
+SELECT * FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+    
+// File format in Queries
+CREATE OR REPLACE STAGE MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+    url = 's3://snowflakeparquetdemo'  
+    
+SELECT * 
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+(file_format => 'MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT')
+
+// Quotes can be omitted in case of the current namespace
+USE MANAGE_DB.FILE_FORMATS;
+
+SELECT * 
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+(file_format => MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT)
+
+CREATE OR REPLACE STAGE MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE
+    url = 's3://snowflakeparquetdemo'   
+    FILE_FORMAT = MANAGE_DB.FILE_FORMATS.PARQUET_FORMAT;
+
+// Syntax for Querying unstructured data
+SELECT 
+    $1:__index_level_0__,
+    $1:cat_id,
+    $1:date,
+    $1:"__index_level_0__",
+    $1:"cat_id",
+    $1:"d",
+    $1:"date",
+    $1:"dept_id",
+    $1:"id",
+    $1:"item_id",
+    $1:"state_id",
+    $1:"store_id",
+    $1:"value"
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+
+// Date conversion 
+SELECT 1;
+
+SELECT DATE(365*60*60*24);
+
+// Querying with conversions and aliases  
+SELECT 
+    $1:__index_level_0__::INT AS index_level,
+    $1:cat_id::VARCHAR(50) AS category,
+    DATE($1:DATE::INT ) AS Date,
+    $1:"dept_id"::VARCHAR(50) AS Dept_ID,
+    $1:"id"::VARCHAR(50) AS ID,
+    $1:"item_id"::VARCHAR(50) AS Item_ID,
+    $1:"state_id"::VARCHAR(50) AS State_ID,
+    $1:"store_id"::VARCHAR(50) AS Store_ID,
+    $1:"value"::INT AS value
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+
+
+// 4.7 Loading PARQUET Data
+  
+// Adding metadata
+SELECT 
+    $1:__index_level_0__::INT AS index_level,
+    $1:cat_id::VARCHAR(50) AS category,
+    DATE($1:DATE::INT ) AS Date,
+    $1:"dept_id"::VARCHAR(50) AS Dept_ID,
+    $1:"id"::VARCHAR(50) AS ID,
+    $1:"item_id"::VARCHAR(50) AS Item_ID,
+    $1:"state_id"::VARCHAR(50) AS State_ID,
+    $1:"store_id"::VARCHAR(50) AS Store_ID,
+    $1:"value"::INT AS value,
+    METADATA$FILENAME AS FILENAME,
+    METADATA$FILE_ROW_NUMBER AS ROWNUMBER,
+    TO_TIMESTAMP_NTZ(current_timestamp) AS LOAD_DATE
+FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE;
+
+SELECT TO_TIMESTAMP_NTZ(current_timestamp)
+
+// Create destination table
+CREATE OR REPLACE TABLE OUR_FIRST_DB.PUBLIC.PARQUET_DATA (
+    ROW_NUMBER INT,
+    index_level INT,
+    cat_id VARCHAR(50),
+    date DATE,
+    dept_id VARCHAR(50),
+    id VARCHAR(50),
+    item_id VARCHAR(50),
+    state_id VARCHAR(50),
+    store_id VARCHAR(50),
+    value INT,
+    Load_date timestamp DEFAULT TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP))
+
+// Load the parquet data
+COPY INTO OUR_FIRST_DB.PUBLIC.PARQUET_DATA
+    FROM (
+        SELECT 
+            METADATA$FILE_ROW_NUMBER,
+            $1:__index_level_0__::INT,
+            $1:cat_id::VARCHAR(50),
+            DATE($1:date::INT),
+            $1:"dept_id"::VARCHAR(50),
+            $1:"id"::VARCHAR(50),
+            $1:"item_id"::VARCHAR(50),
+            $1:"state_id"::VARCHAR(50),
+            $1:"store_id"::VARCHAR(50),
+            $1:"value"::INT,
+            TO_TIMESTAMP_NTZ(CURRENT_TIMESTAMP)
+        FROM @MANAGE_DB.EXTERNAL_STAGES.PARQUETSTAGE);
+            
+SELECT * FROM OUR_FIRST_DB.PUBLIC.PARQUET_DATA;
+
